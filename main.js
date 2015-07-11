91efa942338f2aaacd0ca6b64281615afe99b3e9
Adam Williams <awilli1186@gmail.com>
2015-07-11 11:53:58 -0500

    added css and scss files to repo

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-var test ="sample";
+var $calBtn = document.getElementById('calculateBtn');
+
+var calBtnClickHandler = function(x, y){
+  var x =parseInt(document.getElementById('num_one').value);
+  var y = parseInt(document.getElementById('num_two').value);
+  var total = x * y;
+    if (isNaN(x) || isNaN(y)){
+       alert("enter a number!")
+    } else {
+    return total,
+
+    document.getElementById('answer').innerHTML = total;
+    }
+  };
+
+$calBtn.onclick = calBtnClickHandler;
